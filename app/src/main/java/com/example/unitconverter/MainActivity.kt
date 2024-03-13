0c2ce7e74a86aea80a54c069822a1dc47e237776
NawafAlGhodyah <nwaf557@gmail.com>
2024-03-13 15:35:44 +0300

    77. Day 5 - Button, onClick and Toast

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.unitconverter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -13,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
@@ -39,10 +42,19 @@ fun UnitConverter(){
     Column {
         Text("Unit converter")
         OutlinedTextField(value ="" , onValueChange = {})
-        Text(text = "Results:")
-        Row {
 
+
+
+        Row {
+            //this context is for when an action must happened with in a compose and will be passed to a Toast
+            val context = LocalContext.current
+            Button(onClick = { Toast.makeText(context,"Thanks for clicking!",Toast.LENGTH_LONG).show() }) {
+                Text(text = "Click me!!")
+
+
+            }
         }
+        Text(text = "Results:")
     }
 }
 
